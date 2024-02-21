c00c2dedb0537f0331c9bf0787fa0f9d42d502e9
mibanas <sanabi_mohamed@hotmail.fr>
2024-02-21 13:03:07 +0100

    test

---
+++ src/jest.config.ts
@@ -0,0 +1,15 @@
+// jest.config.ts
+import type { Config } from '@jest/types';
+
+const config: Config.InitialOptions = {
+  preset: 'ts-jest',
+  testEnvironment: 'node',
+  transform: {
+    '^.+\\.tsx?$': 'ts-jest',
+  },
+  moduleNameMapper: {
+    '^@/(.*)$': '<rootDir>/src/$1',
+  },
+};
+
+export default config;
